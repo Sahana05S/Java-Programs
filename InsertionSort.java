b7cb44511911c23518f67418571e3b660e85b74e
Sahana05S <hoopakappa@gmail.com>
2025-08-12 04:52:08 +0530

    insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        for (int i = 1; i < arr.length; i++) {
+            int  val= arr[i];//make a duplicate of the current element
+            int j = i - 1;//to iterate to previous values
+            while (j >= 0 && arr[j] > val) {
+                arr[j + 1] = arr[j];//to swap the values
+                j--;//reduce to check the previous elements whether they are sorted too
+            }
+            arr[j + 1] = val;
+        }
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
