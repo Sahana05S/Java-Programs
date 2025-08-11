85cc18a5afc4c76b478a19401fcb81721c677edb
Sahana05S <hoopakappa@gmail.com>
2025-08-11 16:24:04 +0530

    sorting

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original array:"+Arrays.toString(arr));
+        for(int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
